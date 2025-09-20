1539ddd4bd922ae11ba99bf309b1377267a4a850
juampajuan <jdominguezl@fi.uba.ar>
2025-09-20 18:57:32 +0000

    Ahora el codigo espera que el server le asigne su id

--- tp1/src/upload.py
+++ tp1/src/upload.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import argparse
+import struct
 from socket import *
 import os
 
 CHUNK = 1024 #Mas adelante lo hacemos configurable
+OPERATION = b'U'
+USERID_INICIAL = 0xFFFF
 
 def parse_args():
     p = argparse.ArgumentParser(description = "Cliente Upload (UDP, version mínima)")
@@ -16,25 +19,39 @@ def parse_args():
 
     return p.parse_args()
 
-def upload_stop_and_wait(clientsocket, server, src, name, verbose):
 
-    # Envia encabezado simple (texto) con intencion de upload
-    # protocolo minimo: "UPLOAD <nombre> <tamaño> \n"
+def handshake(clientsocket,server, src, name):
+    
+    # Arma y envia el paquete inicial con la operación y el user_id inicial hacia el server.
 
-    header = f"UPLOAD {name} {os.path.getsize(src)} \n"
+    payload = f"UPLOAD {name} {os.path.getsize(src)} \n".encode()
+    paquete = struct.pack("!H1s", USERID_INICIAL, OPERATION) + payload
+    clientsocket.sendto(paquete, server)
 
-    clientsocket.sendto(header.encode(), server)
+    # Recibo el user_id asignado por el server
+    data, addr = clientsocket.recvfrom(2)
+    user_id, = struct.unpack("!H", data)
+    print(f"User ID asignado por el servidor: {user_id}\n")
+    return user_id
+
+
+
+def upload_stop_and_wait(clientsocket, user_id, server, src, name):
 
     # Envia el archivo en bloques de tamaño CHUNK (sin confiabilidad por ahora)
 
     sent = 0
     with open(src, "rb") as f:
         while True:
-            chunk = f.read(CHUNK)
-            if not chunk:
+            payload = f.read(CHUNK)
+            if not payload:
                 break
-            clientsocket.sendto(chunk, server)
-            sent += len(chunk)
+
+            # Armo y envio el paquete con el user_id, operación y payload
+            paquete = struct.pack("!H1s", user_id, OPERATION) + payload
+            clientsocket.sendto(paquete, server)
+            
+            sent += len(payload)
             
     print(f"\rEnviado {sent} bytes\n")  
      
@@ -53,8 +70,10 @@ def main():
     # abro socket UDP IPv4
     clientsocket = socket(AF_INET, SOCK_DGRAM)
 
+    user_id = handshake(clientsocket, server, args.src, args.name)
+
     if args.protocol == "sw":
-            upload_stop_and_wait(clientsocket, server, args.src, args.name, args.verbose)
+            upload_stop_and_wait(clientsocket, user_id, server, args.src, args.name)
     elif args.protocol == "sr":
             print("Protocolo Selective Repeat no implementado todavia")
     else:
