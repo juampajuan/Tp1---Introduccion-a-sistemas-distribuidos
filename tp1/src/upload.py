b4b115bb502d7a317c3fa068cb9621dd225e9a55
juampajuan <jdominguezl@fi.uba.ar>
2025-09-22 18:50:37 +0000

    Agrego stop and wait y actualizo la forma de hacer el handshake

--- tp1/src/upload.py
+++ tp1/src/upload.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import argparse
 import struct
-from socket import *
 import os
+from socket import *
+from constants import MAX_PACKET_SIZE, FLAGS_SIZE, USERID_SIZE, PAYLOAD_SIZE
+from packet import Packet
 
-CHUNK = 1024 #Mas adelante lo hacemos configurable
-OPERATION = b'U'
-USERID_INICIAL = 0xFFFF
+#constantes
+USERID_INICIAL = 65535  # User ID inicial para la handshake
+TIMEOUT = 2  # Timeout en segundos
+MAX_RETRIES = 5  # Número máximo de reintentos para enviar un paquete
 
 def parse_args():
     p = argparse.ArgumentParser(description = "Cliente Upload (UDP, version mínima)")
@@ -20,40 +23,74 @@ def parse_args():
     return p.parse_args()
 
 
-def handshake(clientsocket,server, src, name):
+def handshake_with_server(clientsocket,server):
     
-    # Arma y envia el paquete inicial con la operación y el user_id inicial hacia el server.
+    # Arma y envia el paquete inicial con user_id=65535, flags CONNECT|SYN
+    try:
+        out_packet = Packet(USERID_INICIAL, flags=Packet.FLAG_CONNECT | Packet.FLAG_SYN)
+        clientsocket.sendto(out_packet.toBytes, server)
 
-    payload = f"UPLOAD {name} {os.path.getsize(src)} \n".encode()
-    paquete = struct.pack("H1s", USERID_INICIAL, OPERATION) + payload
-    clientsocket.sendto(paquete, server)
+        # Recibo el user_id asignado por el server
+        data, _ = clientsocket.recvfrom(MAX_PACKET_SIZE) 
+        in_packet = Packet.from_bytes(data)
+        user_id = in_packet.userId
+        print(f"User ID asignado por el servidor: {user_id}\n")
 
-    # Recibo el user_id asignado por el server
-    data, addr = clientsocket.recvfrom(2)
-    user_id, = struct.unpack("H", data)
-    print(f"User ID asignado por el servidor: {user_id}\n")
-    return user_id
+        ack_packet = Packet(user_id, flags=Packet.FLAG_ACK) 
+        clientsocket.sendto(ack_packet.toBytes, server)
+
+        return user_id
+    
+    except Exception as e:
+        print(f"Error durante el handshake con el servidor: {e}")
+        exit(1)
 
 
 
-def upload_stop_and_wait(clientsocket, user_id, server, src, name):
+def upload_stop_and_wait(clientsocket, user_id, server, src):
 
-    # Envia el archivo en bloques de tamaño CHUNK (sin confiabilidad por ahora)
-
+    # Envia el archivo en bloques de tamaño PAYLOAD_SIZE
+    clientsocket.settimeout(TIMEOUT)  # Timeout de 5 segundos
+    seq = 0
     sent = 0
-    with open(src, "rb") as f:
-        while True:
-            payload = f.read(CHUNK)
-            if not payload:
-                break
 
-            # Armo y envio el paquete con el user_id, operación y payload
-            paquete = struct.pack("H1s", user_id, OPERATION) + payload
-            clientsocket.sendto(paquete, server)
-            
-            sent += len(payload)
-            
-    print(f"\rEnviado {sent} bytes\n")  
+    try:
+        with open(src, "rb") as f:
+            while True:
+                # Armo y envio el paquete con el user_id y data
+                payload = f.read(PAYLOAD_SIZE)
+                is_last = (payload == b'')
+                packet_flag = Packet.FLAG_FIN if is_last else Packet.FLAG_DATA
+                packet = Packet(user_id, payload, flags = packet_flag) #Falta agregar numero de seq
+
+                retries = 0
+
+                while True:
+                    clientsocket.recvfrom(packet.toBytes,server) 
+                    try:
+                        data, _ = clientsocket.recvfrom(MAX_PACKET_SIZE)
+                        ack_packet = Packet.from_bytes(data)
+
+                        if(ack_packet.ack and ack_packet.userId == user_id):
+                            break  # ACK válido recibido, salir del bucle de reintentos 
+                    except socket.timeout:
+                        pass
+
+                    retries += 1
+
+                    if retries > MAX_RETRIES:
+                        print("Número máximo de reintentos alcanzado. Abortando.")
+                        return
+
+                if is_last:
+                    break  # Archivo completamente enviado
+                
+                sent += len(payload)
+                seq ^= 1  # Alterna el número de secuencia entre 0 y 1
+
+    finally:
+        clientsocket.settimeout(None)  # Desactivo el timeout
+    print(f"Archivo enviado correctamente. Total de bytes enviados: {sent}")
      
 
 
@@ -67,19 +104,16 @@ def main():
 
     server = (args.host, args.port)
 
-    # abro socket UDP IPv4
-    clientsocket = socket(AF_INET, SOCK_DGRAM)
+    with socket(AF_INET, SOCK_DGRAM) as clientsocket:
 
-    user_id = handshake(clientsocket, server, args.src, args.name)
+        user_id = handshake_with_server(clientsocket, server)
 
-    if args.protocol == "sw":
-            upload_stop_and_wait(clientsocket, user_id, server, args.src, args.name)
-    elif args.protocol == "sr":
-            print("Protocolo Selective Repeat no implementado todavia")
-    else:
-            print("Protocolo desconocido")
-    
-    clientsocket.close()
+        protocolos = { "sw": upload_stop_and_wait, "sr": lambda *a: print("Protocolo Selective Repeat no implementado todavia") }
+
+        if args.protocol in protocolos:
+            protocolos[args.protocol](clientsocket, user_id, server, args.src)
+        else:
+                print("Protocolo desconocido")
     
 
 
