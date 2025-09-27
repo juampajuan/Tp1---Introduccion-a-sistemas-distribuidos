0146bdb98ca1bbe13bd19c7c48f1a27cb8d9bddd
nico <nrogica@fi.uba.ar>
2025-09-27 18:07:37 -0300

    se modifica handshake del servidor, el primer mensaje del cliente incluye en el payload los datos del servicio solicitado, path, nombre de archivo y mtu, server valida y devuelve ok y su mtu o el error

--- src/start-server/constants.py
+++ src/start-server/constants.py
@@ -11,3 +11,21 @@ PACKET_HEADER_SIZE = (FLAGS_SIZE +
 MAX_PAYLOAD_SIZE = (SERVER_MTU -
                     PACKET_HEADER_SIZE -
                     28)  # 28 bytes para cabecera IP/UDP
+
+
+VALIDACION_OK = 0
+ERROR_FALTA_CAMPO = 1
+ERROR_SERVICIO_INVALIDO = 2
+ERROR_PATH_INCORRECTO = 3
+ERROR_NOMBRE_INVALIDO = 4
+ERROR_MTU_INVALIDO = 5
+
+# Lista de caracteres no permitidos para nombres de archivo en Linux
+CARACTERES_NO_PERMITIDOS = ['/', '\\', '\0', '*', '?', ':', '<', '>', '|', '"']
+
+# Lista de nombres reservados para archivos en Linux (por conflicto o confusión)
+NOMBRES_RESERVADOS = [
+    '.', '..', 'bin', 'boot', 'dev', 'etc', 'home', 'lib', 'media', 'mnt', 'opt',
+    'proc', 'root', 'run', 'sbin', 'srv', 'sys', 'tmp', 'usr', 'var',
+    'ls', 'cat', 'echo', 'sh', 'bash', 'python', 'init', 'systemd', 'rc', 'service'
+]
